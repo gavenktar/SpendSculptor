ddde370ac853d38d3aea8e6e9f8f3309bbb5fabc
Kiryl Arol <kiry.arol@gmail.com>
2023-10-25 21:09:02 +0300

    initital commit

---
+++ src/main/java/by/kirylarol/spendsculptor/SpendSculptorApplication.java
@@ -0,0 +1,13 @@
+package by.kirylarol.spendsculptor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpendSculptorApplication {
+
+    public static void main(String[] args) {
+
+    }
+
+}
